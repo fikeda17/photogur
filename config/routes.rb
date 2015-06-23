c0a2324da703576c99ee002b7988a771a6968c05
Fernando Ikeda <f_ikeda10@hotmail.com>
2015-06-23 16:45:01 -0400

    Making each picture show at its own URL with dynamic routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 get "pictures" => "pictures#index"
+get "pictures/:id" => "pictures#show", as: "picture"
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
