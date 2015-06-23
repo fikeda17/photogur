7db400cba360ecb208c9f36121e1310b2bc4d076
Fernando Ikeda <f_ikeda10@hotmail.com>
2015-06-23 17:11:33 -0400

    Adding feature to create new picture using HTML form

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 get "pictures" => "pictures#index"
+
+post "pictures" => "pictures#create"
+get "pictures/new" => "pictures#new"
+
 get "pictures/:id" => "pictures#show", as: "picture"
+
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
