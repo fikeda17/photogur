19859f4bd5c80248438162e252b249a1a5ac7302
Fernando Ikeda <f_ikeda10@hotmail.com>
2015-06-25 16:46:16 -0400

    Display most recent pictures - using scope

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,2 +1,8 @@
 class Picture < ActiveRecord::Base
+  scope :newest_first, -> { order("created_at DESC") }
+  scope :most_recent_five, -> { newest_first.limit(5) }
+
+  def self.create_before(time)
+  	where("created_at < ?", time)
+  end
 end
